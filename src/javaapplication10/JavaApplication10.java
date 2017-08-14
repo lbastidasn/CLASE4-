7a1f9be3eaa9cae1c85f79607fe0f55c68e11f98
Estudiante <Estudiante@10.203.2.153>
2017-08-14 16:13:03 -0500

    f

---
+++ src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+import java.util.*;
+/**
+ *
+ * @author Estudiante
+ */
+public class JavaApplication10 {
+
+    int i =0;
+    int numero;
+    int resultado=0;
+    
+    
+    public static void main(String[] args) {
+        
+ 
+        Scanner numeroEnt=
+                new Scanner (System.in);
+ 
+        // TODO code application logic here
+    }
+    
+    public static int main;{
+    
+    System.out.println(i);
+    {if (numero>0){ i=i+1;}
+    else {
+    numero=numero+resultado;
+    System.out.println(resultado);
+    }
+    }
+    }
+}
+
